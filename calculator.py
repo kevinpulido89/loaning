d1e8288b1da05b2bcf829f9637afe0509cad4166
Kevin Pulido <kevinp8001@gmail.com>
2023-02-25 18:06:11 -0500

    [K] Rate calculator (#1)
    * Create calculator.py
    
    First version of calculator compound interest
    
    * final calculator rate and method
    
    * Added streamlit frontend
    
    * Modified to accept st.secrets
    
    * fix error publish data
    
    * fix minor changes

---
+++ calculator.py
@@ -0,0 +1,37 @@
+"""
+script to find compound interest for given values.
+"""
+
+
+def calculate_compound_interest(deuda: int, rate: float, time: float) -> float:
+    """
+    Calculates compound interest
+
+    Args:
+        deuda (float): _description_
+        rate (float): _description_
+        time (int): _description_
+
+    Returns:
+        float: _description_
+    """
+
+    pago = deuda * (pow((1 + rate / 100), time))
+    return round(pago - deuda)
+
+
+def compound_interest(deuda: int, rate: float, time: int):
+    """doc"""
+    intereses = []
+    base = deuda / time
+    saldo = deuda
+
+    for _ in range(time):
+        CI = calculate_compound_interest(deuda=saldo, rate=rate, time=1.01)
+        intereses.append(CI)
+        saldo = saldo - base + CI
+
+    cuota_mensual = (sum(intereses) / time) + base
+    intereses_generados = sum(intereses)
+
+    return cuota_mensual, intereses_generados
---
+++ main.py
@@ -0,0 +1,78 @@
+"""
+Script para frontend de cálculo de préstamos
+"""
+from dataclasses import dataclass
+
+import pandas as pd
+import streamlit as st
+from calculator import compound_interest
+
+
+@dataclass
+class Main:
+    """
+    Clase principal para crear interfaz de la calculadora de préstamos
+    """
+
+    titulo: str
+
+    def run(self) -> None:
+        st.set_page_config(page_title="Simulador préstamos", page_icon="💰")
+        st.title(self.titulo)
+
+        vlr_solicitado = st.number_input(
+            label="Valor a pedir prestado (COP)",
+            min_value=500000,
+            max_value=3000000,
+            value=500000,
+            step=20000,
+            format=None,
+            key=None,
+            help="Ingrese el valor que desea pedir prestado (COP):",
+        )
+
+        plazo = st.slider(
+            label="Plazo a diferir (meses):",
+            min_value=1,
+            max_value=18,
+            value=2,
+            help="Ingrese el plazo en el que quiere diferir su préstamo",
+        )
+        if st.button("Simular..."):
+            tasa = self.get_tasa_from_spreadsheets()
+            data = {"deuda": int(vlr_solicitado), "rate": tasa, "time": int(plazo)}
+            # st.write(data)
+
+            self.calcular_valores(**data)
+
+            # st.text(f"{round(self.intereses_generados)=}")
+
+            st.text(f"El plan de pago propuesto sería:")
+            st.info(
+                # f"cuotas de ${round(self.cuota_mensual)} por {data['time']} meses"
+                f"{data['time']} cuotas de ${self.cuota_mensual:,}"
+            )
+
+            st.warning(
+                "📲 Para mayor información puede llamar o enviar Whatsapp al (+57) 3004273839.\
+                Si desea por favor adjunte un pantallazo de esta simulación"
+            )
+
+    def calcular_valores(self, **kwargs):
+        self.cuota_mensual, self.intereses_generados = compound_interest(**kwargs)
+        self.cuota_mensual = round(self.cuota_mensual)
+
+    def get_tasa_from_spreadsheets(self) -> float:
+        try:
+            google_sheet_id = st.secrets["google_sheet_id"]
+            URL = f"https://docs.google.com/spreadsheets/d/{google_sheet_id}/gviz/tq?tqx=out:csv&sheet=nm"
+            df = pd.read_csv(URL)
+            df = df.dropna(axis=1, how="all")
+            return float(df.tasa.values[0])
+        except Exception:
+            st.write("tasa offline")
+            return 6.4
+
+
+if __name__ == "__main__":
+    Main(titulo="Simulador de préstamos 💰").run()
