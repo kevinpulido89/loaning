e9bb6d46b4424301478d995e37334015df9b5048
Kevin Pulido <kevinp8001@gmail.com>
2023-07-29 11:39:14 -0500

    Improve and cache support
    Added Cache support

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 """ Script para frontend de cálculo de préstamos """
+import sys
 import pandas as pd
 import streamlit as st
-
 from calculator import compound_interest
+
+st.set_page_config(page_title="Simulador préstamos", page_icon="💰")
 
 
 class Main:
     """ Crea interfaz de la calculadora de préstamos"""
-    tope_minimo = 375_000
-    tope_maximo = 3_500_000
+
+    def __init__(self):
+        self.tasa, self.tope_minimo, self.tope_maximo = self.get_data_from_spreadsheets()
 
     def run(self) -> None:
-        st.set_page_config(page_title="Simulador préstamos", page_icon="💰")
         st.title("Simulador de préstamos 💰")
+        st.write(sys.version)
 
         vlr_solicitado = st.number_input(
             label="Valor a pedir prestado (COP):",
             min_value=self.tope_minimo,
             max_value=self.tope_maximo,
             value=self.tope_minimo,
-            step=25_000,
-            help=f"Ingrese el valor que desea pedir prestado entre {self.tope_minimo:,} y {self.tope_maximo:,} COP",
+            step=50_000,
+            help=f"Ingrese el valor que desea pedir prestado. Entre {self.tope_minimo:,} y {self.tope_maximo:,} COP",
         )
 
         plazo = st.slider(
@@ -28,7 +31,7 @@ class Main:
             min_value=1,
             max_value=18,
             value=3,
-            help="Elija el plazo en meses el que quiere diferir su préstamo",
+            help="Elija la cantidad de meses en los que quiere diferir su préstamo",
         )
 
         if st.button("Simular:"):
@@ -39,14 +42,16 @@ class Main:
                 st.error(f"El valor solicitado es inferior al mínimo permitido (${self.tope_minimo:,})", icon="🚨")
                 st.stop()
             else:
-                tasa = self.get_tasa_from_spreadsheets()
                 if vlr_solicitado > 2000000:
-                    tasa -= 2.0
+                    self.tasa -= 2.0
                 elif vlr_solicitado > 1000000:
-                    tasa -= 1.0
+                    self.tasa -= 1.0
 
-                data = {"deuda": int(vlr_solicitado), "rate": tasa, "time": int(plazo)}
-                self.calcular_valores(**data)
+                if plazo >= 12:
+                    self.tasa = self.tasa * 1.2
+
+                data = {"deuda": int(vlr_solicitado), "rate": self.tasa, "time": int(plazo)}
+                self.cuota_mensual, self.intereses_generados = self.calcular_valores(**data)
 
                 # data.update({"intereses generados": self.intereses_generados})
                 # st.write(data)
@@ -60,22 +65,25 @@ class Main:
                     icon="📲",
                 )
 
+    @st.cache_data
     def calcular_valores(self, **kwargs):
-        self.cuota_mensual, self.intereses_generados = compound_interest(**kwargs)
-        self.cuota_mensual = round(self.cuota_mensual)
+        cuota_mensual, intereses_generados = compound_interest(**kwargs)
+        cuota_mensual = round(cuota_mensual)
 
-    def get_tasa_from_spreadsheets(self) -> float:
+        return cuota_mensual, intereses_generados
+
+    @st.cache_data
+    def get_data_from_spreadsheets(self) -> tuple:
         try:
             google_sheet_id = st.secrets["google_sheet_id"]
             URL = f"https://docs.google.com/spreadsheets/d/{google_sheet_id}/gviz/tq?tqx=out:csv&sheet=nm"
             df = pd.read_csv(URL)
             df = df.dropna(axis=1, how="all")
             df["tasa"] = df["tasa"].str.replace(",",".")
-            return float(df.tasa.values[0])
+            return float(df.tasa.values[0]), int(df.minimo.values[0]), int(df.maximo.values[0])
         except Exception:
-            st.write("tasa offline")
-            return 5.09
-
+            st.write("Data offline")
+            return 5.1, 375_000, 3_750_000
 
 if __name__ == "__main__":
     Main().run()
